347f46ede2be6313346fc5e8dc7753fe06875c55
Kanji Yomoda <kanji.yy@gmail.com>
2017-08-01 10:29:13 -0700

    Wrote Puppy class design

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+=begin
+BREED OF DOG ------------------------
+Golden Retriever
+
+CHARACTERISTICS ----------------
+Size: large
+Friendly: yes
+Trainability: trainable
+Name: varies
+Fur Color: varies
+
+BEHAVIOR -----------------------
+Jump
+Chew
+Run
+Swim
+=end
