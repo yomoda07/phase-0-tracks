782eac071f24c65340ad8220deb53d7ff5eee037
Kanji Yomoda <kanji.yy@gmail.com>
2017-08-02 11:30:33 -0700

    Create a mixin version of module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,37 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+#
+#   def self.yelling_happily(words)
+#     words + "!! :)"
+#   end
+# end
+#
+# p Shout.yell_angrily("Shoot")
+# p Shout.yelling_happily("Sweet")
+
+
 module Shout
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yelling_happily(words)
+  def yelling_happily(words)
     words + "!! :)"
   end
 end
 
-p Shout.yell_angrily("Shoot")
-p Shout.yelling_happily("Sweet")
+class Human
+  include Shout
+end
+
+class Dog
+  include Shout
+end
+
+human = Human.new
+p human.yelling_happily("Awesome")
+
+dog = Dog.new
+p dog.yell_angrily("Woof")
