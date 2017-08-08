5a4390b730e6e37f8510128dda9e005b79b8af5a
Kanji Yomoda <kanji.yy@gmail.com>
2017-08-07 21:06:45 -0700

    Create a file and pseudocoding to reverse a word

---
+++ js/explore.js
@@ -0,0 +1,7 @@
+// - Psuedcoding
+// define a function that takes string as a parameter.
+// create an new empty string to store reversed word
+// extract each element from left to right in the original string
+//   set the extracted letter plus the new string to the new string
+// return new string
+//
