c088da9d6e03191ff90d147e3fe01806fbd4174b
Kanji Yomoda <kanji.yy@gmail.com>
2017-08-27 21:51:02 -0700

    Create a template for campus

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -1,27 +1,29 @@
 # require gems
 require 'sinatra'
 require 'sqlite3'
-
 set :public_folder, File.dirname(__FILE__) + '/static'
-
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
-
+get '/campus' do
+    @campus = db.execute("SELECT * FROM campus")
+    erb :campus
+end
 # show students on the home page
 get '/' do
   @students = db.execute("SELECT * FROM students")
   erb :home
 end
-
 get '/students/new' do
   erb :new_student
 end
-
 # create new students via
 # a form
 post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
-
 # add static resources
+post '/campus' do
+    db.execute("INSERT INTO campus (name) VALUES (?)", [params['campus']])
+    redirect '/campus'
+end
